a8d6b4d681910cddfa72f8d8818bdf1a75c12f7b
tahmidtonmoy48@gmail.com <tahmidtonmoy48@gmail.com>
2025-08-01 11:41:14 +0600

    Added lab 3

---
+++ Mid/lab3/output.js
@@ -0,0 +1,8 @@
+function showImg() {
+  const output = document.getElementById('output');
+  output.innerHTML = `
+    <div style="width: 200px; margin: auto;">
+      <img src="LAB3/img.png" alt="Sample Image" style="width: 100%; height: auto; border-radius: 10px;">
+    </div>
+  `;
+}
